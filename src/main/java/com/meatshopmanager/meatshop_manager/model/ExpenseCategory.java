81ac8ba6f483969ce1e1b6544b018702f5b7f692
VieiraGuilherme-dev <gv524003@gmail.com>
2026-01-02 14:11:12 -0300

    add expense category enum

---
+++ src/main/java/com/meatshopmanager/meatshop_manager/model/ExpenseCategory.java
@@ -0,0 +1,11 @@
+package com.meatshopmanager.meatshop_manager.model;
+
+public enum ExpenseCategory {
+    SALARY,
+    MERCHANDISE,
+    WATER,
+    ELECTRICITY,
+    RENT,
+    INTERNET,
+    OTHER
+}
