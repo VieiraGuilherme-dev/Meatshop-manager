ce12cf60a33604d2222d2519a867af68d520390f
VieiraGuilherme-dev <gv524003@gmail.com>
2026-01-02 14:02:41 -0300

    Projeto criado

---
+++ src/main/java/com/meatshopmanager/meatshop_manager/MeatshopManagerApplication.java
@@ -0,0 +1,13 @@
+package com.meatshopmanager.meatshop_manager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeatshopManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MeatshopManagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/meatshopmanager/meatshop_manager/MeatshopManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.meatshopmanager.meatshop_manager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeatshopManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
