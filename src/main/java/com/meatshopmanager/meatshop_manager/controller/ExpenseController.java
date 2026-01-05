28266b50f465f7fb168dac12f81a9eb227972ffc
VieiraGuilherme-dev <gv524003@gmail.com>
2026-01-05 18:32:14 -0300

    use dto in expense controller

--- src/main/java/com/meatshopmanager/meatshop_manager/controller/ExpenseController.java
+++ src/main/java/com/meatshopmanager/meatshop_manager/controller/ExpenseController.java
@@ -1,5 +1,7 @@
 package com.meatshopmanager.meatshop_manager.controller;
 
+import com.meatshopmanager.meatshop_manager.dto.ExpenseDTO;
+import com.meatshopmanager.meatshop_manager.mapper.ExpenseMapper;
 import com.meatshopmanager.meatshop_manager.model.Expense;
 import com.meatshopmanager.meatshop_manager.service.ExpenseService;
 import org.springframework.http.ResponseEntity;
@@ -14,36 +16,43 @@ public class ExpenseController {
 
     private final ExpenseService service;
 
-    public ExpenseController(ExpenseService service){
+    public ExpenseController(ExpenseService service) {
         this.service = service;
     }
 
     @PostMapping
-    public ResponseEntity<Expense> create(@RequestBody Expense expense){
-        return  ResponseEntity.ok(service.save(expense));
-
+    public ResponseEntity<ExpenseDTO> create(@RequestBody ExpenseDTO dto) {
+        Expense saved = service.save(ExpenseMapper.toEntity(dto));
+        return ResponseEntity.ok(ExpenseMapper.toDTO(saved));
     }
 
     @GetMapping
-    public ResponseEntity<List<Expense>> findAll(){
-        return ResponseEntity.ok(service.findAll());
+    public ResponseEntity<List<ExpenseDTO>> findAll() {
+        return ResponseEntity.ok(
+                service.findAll()
+                        .stream()
+                        .map(ExpenseMapper::toDTO)
+                        .toList()
+        );
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<Expense> findById(@PathVariable Long id){
-        return ResponseEntity.ok(service.findById(id));
+    public ResponseEntity<ExpenseDTO> findById(@PathVariable Long id) {
+        Expense expense = service.findById(id);
+        return ResponseEntity.ok(ExpenseMapper.toDTO(expense));
     }
 
     @PutMapping("/{id}")
-    public ResponseEntity<Expense> update(
+    public ResponseEntity<ExpenseDTO> update(
             @PathVariable Long id,
-            @RequestBody Expense expense
-    ){
-        return ResponseEntity.ok(service.update(id, expense));
+            @RequestBody ExpenseDTO dto
+    ) {
+        Expense updated = service.update(id, ExpenseMapper.toEntity(dto));
+        return ResponseEntity.ok(ExpenseMapper.toDTO(updated));
     }
 
     @DeleteMapping("/{id}")
-    public ResponseEntity<Void> delete(@PathVariable Long id){
+    public ResponseEntity<Void> delete(@PathVariable Long id) {
         service.delete(id);
         return ResponseEntity.noContent().build();
     }
