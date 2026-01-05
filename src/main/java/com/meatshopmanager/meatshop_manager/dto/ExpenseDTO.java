f2672c56f9a7fb704b2399ac1a335d6d9b4f2c35
VieiraGuilherme-dev <gv524003@gmail.com>
2026-01-05 17:49:57 -0300

    Expense DTO

---
+++ src/main/java/com/meatshopmanager/meatshop_manager/dto/ExpenseDTO.java
@@ -0,0 +1,58 @@
+package com.meatshopmanager.meatshop_manager.dto;
+
+import com.meatshopmanager.meatshop_manager.model.ExpenseCategory;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+public class ExpenseDTO {
+
+    private Long id;
+    private String description;
+    private ExpenseCategory category;
+    private BigDecimal amount;
+    private LocalDate expenseDate;
+
+    public ExpenseDTO() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public ExpenseCategory getCategory() {
+        return category;
+    }
+
+    public void setCategory(ExpenseCategory category) {
+        this.category = category;
+    }
+
+    public BigDecimal getAmount() {
+        return amount;
+    }
+
+    public void setAmount(BigDecimal amount) {
+        this.amount = amount;
+    }
+
+    public LocalDate getExpenseDate() {
+        return expenseDate;
+    }
+
+    public void setExpenseDate(LocalDate expenseDate) {
+        this.expenseDate = expenseDate;
+    }
+}
