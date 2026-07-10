e0b196c632e6fd86c8a5e600d5740f7ab525ed1f
VieiraGuilherme-dev <gv524003@gmail.com>
2026-07-10 20:43:34 -0300

    feat(categoria): add CategoriaRepository with type and name queries

---
+++ src/main/java/com/meatshopmanager/repository/CategoriaRepository.java
@@ -0,0 +1,12 @@
+package com.meatshopmanager.repository;
+
+import com.meatshopmanager.model.Categoria;
+import com.meatshopmanager.model.TipoCategoria;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface CategoriaRepository extends JpaRepository<Categoria, Long> {
+    List<Categoria> findByTipo(TipoCategoria tipo);
+    boolean existByNome(String nome);
+}
